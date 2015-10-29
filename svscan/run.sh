48e882d9ceed64b5d1faa9e7d63088df984b95aa
Christian Senkowski <cs@2scale.net>
2015-10-29 14:18:57 +0100

    fixed typo

--- svscan/run.sh
+++ svscan/run.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 cd ../multilog && go build -o multilog *.go
 cd ../svc && go build -o svc *.go
-cd ../svscan && go build -o svcan *.go
+cd ../svscan && go build -o svscan *.go
 ./svscan -path ~/services
